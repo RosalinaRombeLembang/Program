5c0da55a1308986ef73725854506b5ef0d8ef32e
RosalinaRombeLembang <96455871+RosalinaRombeLembang@users.noreply.github.com>
2022-01-05 21:22:02 -0800

    Add files via upload

---
+++ Menghitung_Luas_dan_keliling_Lingkaran.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Menghitung_Luas_dan_keliling_Lingkaran {
+    public static void main(String[] args) {
+    String Luas_Lingkaran;
+    String Keliling_Ligkaran;
+    Luas_Lingkaran =("phi*r pangkat 2");
+        String Keliling_Lingkaran = "2 phi*r";
+        System.out.println("Luas_keliling(phi*r pangkat 2  )");
+        System.out.println("Keliling_Lingkaran(2 phi*r)");
+        
+              
+    
+    
+    }
+    
+}
